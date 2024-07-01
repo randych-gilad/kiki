1e622a868f1309ecdae6bbe605efdd6168b6df37
randych-gilad <xadaka@gmail.com>
2024-07-01 19:33:31 +0300

    add remaning fields

--- deployment.go
+++ deployment.go
@@ -1,8 +1,28 @@
 package main
 
 type Deployment struct {
-	ApiVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
+	ApiVersion           string            `yaml:"apiVersion"`
+	Kind                 string            `yaml:"kind"`
+	Namespace            string            `yaml:"namespace"`
+	Labels               map[string]string `yaml:"labels"`
+	Annotations          map[string]string `yaml:"annotations"`
+	Replicas             int               `yaml:"replicas"`
+	RevisionHistoryLimit int               `yaml:"revisionHistoryLimit"`
+	Strategy             map[string]string `yaml:"strategy"`
+	ImagePullSecret      string            `yaml:"imagePullSecret"`
+	NodeSelector         string            `yaml:"nodeSelector"`
+	VolumeMounts         map[string]string `yaml:"volumeMounts"`
+	Image                string            `yaml:"image"`
+	Repo                 string
+	Version              string
+	Port                 string                        `yaml:"port"`
+	Resources            *map[string]map[string]string `yaml:"resources"`
+	Probe                map[string]string
+	EnvSecretLocal       []string
+	EnvSecretGlobal      map[string]string
+	EnvSecretSpecific    []string
+	EnvConfigmap         []string
+	EnvPlain             map[string]string
 }
 
 func NewDeployment(kind string) *Deployment {
