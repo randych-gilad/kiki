497eac4dc3d853161b34ad5ffea4c50c3bae9d75
randych-gilad <xadaka@gmail.com>
2024-07-02 17:21:23 +0300

    start adding defaulting

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 )
 
 func main() {
+	v := ValuesParse("EXAMPLE.yaml")
+	if err := v.Default(); err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	// fmt.Printf("%#v\n", v)
 	deployment := NewDeployment(
 		"Deployment",
 		Metadata{})
-
 	yamlData, err := yaml.Marshal(&deployment)
 	if err != nil {
 		slog.Error(err.Error())
@@ -20,7 +25,8 @@ func main() {
 
 	fmt.Println(string(yamlData))
 }
-func ParseValues(filepath string) Values {
+
+func ValuesParse(filepath string) Values {
 	data, err := os.ReadFile(filepath)
 	if err != nil {
 		slog.Error("error reading file: %v", err)
@@ -31,4 +37,31 @@ func ParseValues(filepath string) Values {
 		slog.Error("error unmarshaling YAML: %v", err)
 	}
 	return values
+}
+
+func (v *Values) Default() error {
+	slog.Info("Validate and default values against defined rules...")
+
+	if v.Deployment.Kind == "" {
+		slog.Warn("Deployment.Kind not specified, defaulting to Deployment")
+		v.Deployment.Kind = "Deployment"
+	} else if v.Deployment.Kind != "Deployment" && v.Deployment.Kind != "DaemonSet" {
+		return fmt.Errorf("invalid Deployment.Kind, got: %v, want: Deployment or DaemonSet", v.Deployment.Kind)
+	}
+
+	if v.Deployment.Annotations == nil {
+		slog.Warn("Deployment.Annotations not specified")
+	}
+
+	if v.Deployment.Replicas == 0 {
+		v.Deployment.Replicas = 1
+		slog.Warn("Deployment.Replicas not specified, defaulting to 1")
+	}
+
+	if v.Deployment.Strategy == nil {
+		v.Deployment.Strategy = make(map[string]string)
+		v.Deployment.Strategy["type"] = "RollingUpdate"
+	}
+
+	return nil
 }
